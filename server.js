893ed67b456662511f5500530da41bb25f252db6
Ivan Donato <donato.ivan@gmail.com>
2019-07-09 16:17:59 +1000

    api running

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = new express();
 const PORT = process.env.PORT || 5000;
 
+
 const cors = require('cors');
+
+app.use(express.json())
 
 const mongoPROD_URI =
   "mongodb+srv://admin:123456pop@ballarat-5i5ts.mongodb.net/test?retryWrites=true&w=majority";
 
+  
 mongoose.connect(mongoPROD_URI, { useNewUrlParser: true }, err => {
   if (err) return console.log(`${err}`);
   console.log("connected to mongodb");
 });
+
+
+app.get('/', (req, res) => {
+  res.send('Api running')
+})
 
 app.listen(PORT, () => {
   console.log(`listening on Port ${PORT}`);
