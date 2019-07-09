c612eb2cc1d374e49376dd964017b47d31788bf9
Ivan Donato <donato.ivan@gmail.com>
2019-07-09 12:19:45 +1000

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const app = new express();
-
 const PORT = process.env.PORT || 5000;
+
+const cors = require('cors');
 
 const mongoPROD_URI =
   "mongodb+srv://admin:123456pop@ballarat-5i5ts.mongodb.net/test?retryWrites=true&w=majority";
