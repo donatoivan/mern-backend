5cd10a36d0f32ca30042ca73de5dccd2d91c8db1
Ivan Donato <donato.ivan@gmail.com>
2019-07-17 16:09:37 +1000

    added carousel Images, model, controller, routes and crud functionality

---
+++ controllers/carouselImagesController.js
@@ -0,0 +1,44 @@
+const CarouselImage = require('../models/CarouselImage');
+
+// Create a Carousel Image
+
+const createNewCarouselImage = async (req, res) => {
+  try {
+    const{ image } = req.body;
+    const newImage = await CarouselImage.create({ image })
+    return res.status(201).json(`Successfully created Carousel Image: ${newImage.image}`);
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(`Could not create Carousel Image ${error.message}`)
+  }
+};
+
+// Get All Carousel Images
+const getAllCarouselImages = async (req, res) => {
+  try {
+    const images = await CarouselImage.find();
+    return res.status(200).json(images)
+  } catch (error) {
+    console.log(error.message)
+    return res.status(400).json(`Could not get Carousel Images ${error.message}`)
+  }
+};
+
+// Update one Carousel Image
+
+const updateCarouselImage = async (req, res) => {
+  const { id } = req.params;
+  const payload = req.body;
+  try {
+    const image = await CarouselImage.updateOne({ _id: id}, payload);
+    return res.status(200).json(`successfully update Carousel image`)
+  } catch (error) {
+    return res.status(400).json(`Could not update Carousel Image ${error.message}`)
+  }
+}
+
+module.exports = {
+  createNewCarouselImage,
+  getAllCarouselImages,
+  updateCarouselImage
+}
